3ee0ccbabc68c5af1b382f9ef729e3f1bab5afd1
Eduardo Cuducos <cuducos@gmail.com>
2016-11-01 14:27:01 -0200

    PEP8: code format

--- telegram.py
+++ telegram.py
@@ -1,76 +1,66 @@
-
-from telegram.ext import(Updater, CommandHandler, MessageHandler, Filters, RegexHandler,ConversationHandler)
+from telegram.ext import (CommandHandler, ConversationHandler, Filters,
+                          MessageHandler, Updater)
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
 
 class Katcha_Yakisoba():
     logger = logging.getLogger(__name__)
 
-    START,VALIDADOR_START  = range(2)
+    START, VALIDADOR_START = range(2)
 
-
-
-    def validador_start(self,bot, update):
+    def validador_start(self, bot, update):
 
         msg = update.message.text
         user = update.message.from_user.first_name
 
         type = self.type_msg(update.message.entities)
 
-
         if type == "url":
-            mensagem = """{} \n{}""".format(msg,user)
+            mensagem = "{} \n{}".format(msg, user)
 
             links_postados = open("links.txt", "r").read()
             links_postados = links_postados.split()
+            twitter = "https://twitter.com/KatchaYakisoba"
 
             for verificar_site in links_postados:
                 if msg == verificar_site:
+                    text = "Link ja foi postado, consulte em " + twitter
                     bot.sendMessage(update.message.chat_id,
-                                text="""Link ja foi postado, consulte em https://twitter.com/KatchaYakisoba""",
+                                    text=text,
                                     one_time_keyboard=True)
-                    return self.cancel(bot,update)
+                    return self.cancel(bot, update)
+
             links_postados.append(msg)
             links_postados = " ".join(links_postados)
-            salvar_links_postados = open("links.txt","w").write(links_postados)
+            salvar_links_postados_handler = open("links.txt", "w")
+            salvar_links_postados_handler.write(links_postados)
             self.enviar_twitter(mensagem)
-            bot.sendMessage(update.message.chat_id,
 
-                            text="""Link salvo com sucesso, consulte em https://twitter.com/KatchaYakisoba""",
+            text = "Link salvo com sucesso, consulte em " + twitter
+            bot.sendMessage(update.message.chat_id,
+                            text=text,
                             one_time_keyboard=True)
 
-            return self.cancel(bot,update)
+            return self.cancel(bot, update)
 
-
-
-
-    def start(self,bot, update):
-
-
-
-
+    def start(self, bot, update):
         bot.sendMessage(update.message.chat_id,
-
-                        text="""envie o link""",
-                         one_time_keyboard=True)
-
+                        text="envie o link",
+                        one_time_keyboard=True)
         return self.VALIDADOR_START
 
-
-
-    def cancel(self,bot, update):
+    def cancel(self, bot, update):
         user = update.message.from_user
-
-
-
         return ConversationHandler.END
 
-
-    def error(self,bot, update, error):
+    def error(self, bot, update, error):
         self.logger.warn('Update "%s" caused error "%s"' % (update, error))
-
 
     def __init__(self):
         # Create the EventHandler and pass it your bot's token.
@@ -79,16 +69,15 @@ class Katcha_Yakisoba():
         # Get the dispatcher to register handlers
         dp = updater.dispatcher
 
-        # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+        # Add conversation handler w/ the states GENDER, PHOTO, LOCATION & BIO
+        validator = [MessageHandler([Filters.text], self.validador_start)]
         conv_handler = ConversationHandler(
             entry_points=[CommandHandler('link', self.start)],
-
             states={
                 self.START: [MessageHandler([Filters.text], self.start)],
-                self.VALIDADOR_START: [MessageHandler([Filters.text], self.validador_start)],
+                self.VALIDADOR_START: validator,
 
             },
-
             fallbacks=[CommandHandler('xau', self.cancel)]
         )
 
@@ -100,12 +89,13 @@ class Katcha_Yakisoba():
         # Start the Bot
         updater.start_polling()
 
-        # Run the bot until the you presses Ctrl-C or the process receives SIGINT,
-        # SIGTERM or SIGABRT. This should be used most of the time, since
-        # start_polling() is non-blocking and will stop the bot gracefully.
+        # Run the bot until the you presses Ctrl-C or the process receives
+        # SIGINT, SIGTERM or SIGABRT. This should be used most of the time,
+        # since start_polling() is non-blocking and will stop the bot
+        # gracefully.
         updater.idle()
 
-    def type_msg(self,type):
+    def type_msg(self, type):
         for i in type:
             type = i
         try:
@@ -114,7 +104,7 @@ class Katcha_Yakisoba():
             pass
         return type
 
-    def enviar_twitter(self,msg):
+    def enviar_twitter(self, msg):
         from twython import Twython
 
         APP_KEY = "APP_KEY"
@@ -128,51 +118,26 @@ class Katcha_Yakisoba():
         twitter.update_status(status=msg)
 
 
-
-
 if __name__ == '__main__':
     Katcha_Yakisoba()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 """
-    reply_keyboard = [['SANGRIA', 'FINANCEIRO']]
+reply_keyboard = [['SANGRIA', 'FINANCEIRO']]
 
-        bot.sendMessage(update.message.chat_id,
+    bot.sendMessage(update.message.chat_id,
 
-                        text="Link salvo com sucesso!",
-                        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+                    text="Link salvo com sucesso!",
+                    reply_markup=ReplyKeyboardMarkup(reply_keyboard,
+                                                     one_time_keyboard=True))
 
-        return self.VALIDADOR_START
+    return self.VALIDADOR_START
 
-    def gender(self,bot, update):
-        user = update.message.from_user
-        self.logger.info("Gender of %s: %s" % (user.first_name, update.message.text))
-        bot.sendMessage(update.message.chat_id,
-                        text='I see! Please send me a photo of yourself, '
-                             'so I know what you look like, or send /skip if you don\'t want to.')
-
-return 0"""
+def gender(self,bot, update):
+    user = update.message.from_user
+    self.logger.info("Gender of %s: %s" % (user.first_name,
+                                           update.message.text))
+    text = "I see! Please send me a photo of yourself, so I know what you look
+            like, or send /skip if you don\'t want to."
+    bot.sendMessage(update.message.chat_id, text=text)
+    return 0
+"""
